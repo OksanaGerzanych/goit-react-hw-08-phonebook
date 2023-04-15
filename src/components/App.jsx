cc3740eb1afa0d633a80f0b0395335396db32477
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-04-15 08:12:58 +0300

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -25,9 +27,9 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={ <PrivateRoute  component={<ContactsPage />} redirectTo="/login" />   } />
+          <Route path="/register" element={ <RestrictedRoute  component={<RegisterPage />} redirectTo='/contacts'/> } />
+        <Route path="/login" element={<RestrictedRoute   component={<LoginPage />} redirectTo='/contacts' />}  />
       </Route>
     </Routes>
   
