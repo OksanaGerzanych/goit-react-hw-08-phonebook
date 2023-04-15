aee841cd911327db183f4606cd9dac84ddbf835c
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-04-15 13:07:31 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  const {isRefreshing} = useAuth();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -23,15 +23,32 @@ export const App = () => {
 
   return isRefreshing ? (
     <b>Refreshing user...</b>
-  ) : ( 
+  ) : (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/contacts" element={ <PrivateRoute  component={<ContactsPage />} redirectTo="/login" />   } />
-          <Route path="/register" element={ <RestrictedRoute  component={<RegisterPage />} redirectTo='/contacts'/> } />
-        <Route path="/login" element={<RestrictedRoute   component={<LoginPage />} redirectTo='/contacts' />}  />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
       </Route>
     </Routes>
-  
   );
 };
